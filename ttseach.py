0d9be68d61a3e4345418725f22716448729a4ce5
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-20 23:04:01 +0000

    Initial Commit

--- ttseach.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-import os
-tts = gTTS(text='Good morning', lang='en')
-tts.save("good.mp3")
-os.system("mpg123 good.mp3")
-os.remove('good.mp3')
-
--- wiki.py
+++
@@ -1,6 +0,0 @@
-import wikipedia
-
-while True:
-	input = raw_input("Q: ")
-	wikipedia.set_lang('en')
-	print wikipedia.summary(input,sentences = 2)
